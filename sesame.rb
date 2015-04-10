910b5b38b1f430bfecf53738b0fc4e45477ef2a3
Alex Grant <alex@vermonster.com>
2015-04-10 16:46:57 -0400

    First pass at doorbell logic

--- sesame.rb
+++ sesame.rb
@@ -1,8 +1,24 @@
 require 'bundler'
 Bundler.require
 
-get '/hello-monkey' do
+get '/' do
   Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello Monkey'
+    r.Gather numDigits: 4, action: '/access', finishOnKey: '*' do |g|
+      g.Say 'Enter access code, or press star to call the office.'
+    end
+    r.Say 'Code not entered, disconnecting.'
+  end.text
+end
+
+post '/access' do
+  Twilio::TwiML::Response.new do |r|
+    if params['Digits'] == '1138'
+      r.Say 'Access granted.'
+      r.Play digits: '5'
+    elsif params['Digits'] == ''
+      r.Dial '+16178070857'
+    else
+      r.Say 'Access denied.'
+    end
   end.text
 end
