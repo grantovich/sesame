924ac36b0529a41882b5ea19382dad8e75187995
Alex Grant <alex@vermonster.com>
2015-04-10 17:08:48 -0400

    Add some waits to the unlock

--- sesame.rb
+++ sesame.rb
@@ -14,7 +14,7 @@ post '/access' do
   Twilio::TwiML::Response.new do |r|
     if params['Digits'] == '1138'
       r.Say 'Access granted.'
-      r.Play digits: '5'
+      r.Play digits: '5ww5ww5ww5'
     elsif params['Digits'] == '*'
       r.Dial '+16178070857'
     else
