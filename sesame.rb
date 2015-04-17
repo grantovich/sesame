4950f3f82083ae43ae08a3888c3d50e5026def02
Alex Grant <alex@vermonster.com>
2015-04-17 15:49:44 -0400

    Expand slash command functionality

--- sesame.rb
+++ sesame.rb
@@ -2,11 +2,19 @@ require 'json'
 require 'date'
 require 'bundler'
 Bundler.require
-$stdout.sync = true
+Chronic.time_class = DateTime
+DATETIME_FORMAT = '%Y-%m-%d at %I:%M%P'
 
 redis = Redis.new(url: ENV['REDISTOGO_URL'])
+redis.set('codes', '[]') # TODO: Remove this
 codes = JSON.parse(redis.get('codes') || '[]').map do |code|
-  { code: code['code'], expires: DateTime.parse(code['expires']) }
+  {
+    code: code['code'],
+    begins: DateTime.parse(code['begins']),
+    expires: DateTime.parse(code['expires']),
+    creator: code['creator'],
+    label: code['label']
+  }
 end
 
 before do
@@ -30,7 +38,7 @@ end
 
 post '/access' do
   Twilio::TwiML::Response.new do |r|
-    if codes.any?{ |code| code[:code] == params['Digits'] }
+    if codes.any?{ |code| code[:code] == params['Digits'] && code[:begins] < DateTime.now }
       r.Say 'Access granted.'
       r.Play digits: '5ww5ww5ww5'
     elsif params['Digits'] == '*'
@@ -41,10 +49,52 @@ post '/access' do
   end.text
 end
 
-post '/generate' do
-  if params['token'] == ENV['SLASH_COMMAND_TOKEN']
-    new_code = rand(10000).to_s.rjust(4, '0') until codes.none?{ |code| code[:code] == new_code }
-    codes.push({ code: new_code, expires: Chronic.parse('5 minutes from now') })
-    "Generated new access code #{new_code}, expires #{codes.last[:expires]}"
+post '/command' do
+  return status 401 unless params['token'] == ENV['SLASH_COMMAND_TOKEN']
+  command = params['text'] || ''
+
+  if command.strip == 'list'
+
+    codes.map do |code|
+      [
+        code[:code],
+        "Begins #{code[:begins].strftime(DATETIME_FORMAT)}",
+        "Expires #{code[:expires].strftime(DATETIME_FORMAT)}",
+        "Created by #{code[:creator]}",
+        code[:label] || '(no label)'
+      ].join(' – ')
+    end.join("\n")
+
+  elsif command =~ /from|until|for/
+
+    begins = Chronic.parse(command[/from (.*)( until| for|$)/, 1]) || DateTime.now
+    expires = Chronic.parse(command[/until (.*)( for|$)/, 1]) || Chronic.parse('15 minutes from now')
+    label = command[/for (.*)/, 1]
+
+    new_code = loop do
+      random_code = rand(10000).to_s.rjust(4, '0')
+      break random_code unless codes.any?{ |code| code[:code] == random_code }
+    end
+
+    codes.push({
+      code: new_code,
+      begins: begins,
+      expires: expires,
+      creator: params['user_name'],
+      label: label
+    })
+
+    "Generated access code #{new_code}, begins #{begins.strftime(DATETIME_FORMAT)}, expires #{expires.strftime(DATETIME_FORMAT)}"
+
+  elsif command.strip =~ /^revoke \d{4}$/
+
+    # TODO: Handle revoke
+
+  else
+    [
+      "List current access codes: /sesame list",
+      "Generate new access code: /sesame [from <datetime>] [until <datetime>] [for <label>]",
+      "Revoke existing access code: /sesame revoke <code>"
+    ].join("\n")
   end
 end
